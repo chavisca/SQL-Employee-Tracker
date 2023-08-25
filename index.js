75ba7faae94732aa809ad938771827ad2e3f1bc9
Charles Chavis <chavisca@gmail.com>
2023-08-25 16:38:47 -0400

    created table schema and .env config

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const choiceList = ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role'];
+
 const questions = [
     {
-        type: 'input',
+        type: 'list',
         message: 'What would you like to do?',
         name: 'startChoice',
+        choices: choiceList,
     },
   ];
+
+  function init() {
+    inquirer.prompt(questions)
+    .then (response => {
+
+    });
+  };
+
+  init();
