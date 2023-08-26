dff3c8519101ffe2ea620699ee30eba59fc2b493
Charles Chavis <chavisca@gmail.com>
2023-08-26 03:46:23 -0400

    added switch for inquirer prompt responses, build multiple functions for query addition/creation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const startProcess = require('./startProcess');
 
 const choiceList = ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role'];
 
@@ -7,16 +8,21 @@ const questions = [
     {
         type: 'list',
         message: 'What would you like to do?',
-        name: 'startChoice',
+        name: 'initChoice',
         choices: choiceList,
-    },
+    }
   ];
 
   function init() {
     inquirer.prompt(questions)
-    .then (response => {
+    .then((answers) => {
+        const startChoice = startProcess(initChoice);
+
+    })
 
     });
   };
+
+
 
   init();
---
+++ startProcess.js
@@ -0,0 +1,218 @@
+const addADept = [
+    {
+        type: 'input',
+        message:  'What is the NAME of the department?',
+        name: 'deptName'
+    },
+    {
+        type: 'input',
+        message:  'What is the ID of the department?',
+        name: 'deptID'
+    }
+];
+
+const addARole = [
+    {
+        type: 'input',
+        message:  'What is the NAME of the role?',
+        name: 'roleName'
+    },
+    {
+        type: 'input',
+        message:  'What is the ID of the role?',
+        name: 'roleID'
+    },
+    {
+        type: 'input',
+        message:  'What is the SALARY of the role?',
+        name: 'roleSalary'
+    },
+    {
+        type: 'input',
+        message:  'What is the DEPARTMENT ID of the role?',
+        name: 'roleDeptID'
+    }
+];
+
+const addAnEmp = [
+    {
+        type: 'input',
+        message:  'What is the FIRST NAME of the employee?',
+        name: 'empFirstName'
+    },
+    {
+        type: 'input',
+        message:  'What is the LAST NAME of the employee?',
+        name: 'empLastName'
+    },
+    {
+        type: 'input',
+        message:  'What is the ID NUMBER of the employee?',
+        name: 'empID'
+    },
+    {
+        type: 'input',
+        message:  'What is the ROLE ID of the employee?',
+        name: 'empRoleID'
+    },
+    {
+        type: 'input',
+        message:  'What is the MANAGER ID of the employee?',
+        name: 'empMgrID'
+    }
+];
+// Role update still needs building
+const updateRoleLookup = [
+    {
+        type: 'input',
+        message:  'What is the ROLE ID you want to update?',
+        name: 'updtID'
+    }
+];
+
+const updateRole = [
+    {
+        type: 'input',
+        message:  'What is the TITLE of the role?',
+        name: 'updtTitle'
+    },
+    {
+        type: 'input',
+        message:  'What is the SALARY of the role?',
+        name: 'updtSalary'
+    },
+    {
+        type: 'input',
+        message:  'What is the DEPARTMENT ID of the role?',
+        name: 'updtDeptID'
+    }
+]
+
+function startProcess(initChoice) {
+    let query;
+
+    switch(initChoice) {
+        case "View All Departments": 
+            query = "SELECT * FROM departments";
+            connection.query(query, (err, results) => {
+                if (err) {
+                    console.error('Error executing query:', err);
+                    return;
+                }
+                console.log('All Departmeents:', results);
+            });
+            break;
+        case "View All Roles":
+            query = "SELECT * FROM roles";
+            connection.query(query, (err, results) => {
+                if (err) {
+                    console.error('Error executing query:', err);
+                    return;
+                }
+                console.log('All Roles:', results);
+            });
+            break;
+        case "View All Employees":
+            query = "SELECT * FROM employees";
+            connection.query(query, (err, results) => {
+                if (err) {
+                    console.error('Error executing query:', err);
+                    return;
+                }
+                console.log('All Roles:', results);
+            });
+            break;
+        case "Add A Department":
+            query = inquirer.prompt(addADept)
+            .then((answers) => {
+                const deptName = answers.deptName;
+                const deptID = answers.deptID;
+                addDepartmentToDatabase(deptName, deptID);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+            return deptName, deptID;
+        case "Add A Role":
+            query = inquirer.prompt(addARole)
+            .then((answers) => {
+                const roleName = answers.roleName;
+                const roleID = answers.roleID;
+                const roleSalary = answers.roleSalary;
+                const roleDeptID = answers.roleDeptID;
+                addRoleToDatabase(roleName, roleID, roleSalary, roleDeptID);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+            return roleName, roleID, roleSalary, roleDeptID;
+        case "Add An Employee":
+            query = inquirer.prompt(addAnEmp)
+            .then((answers) => {
+                const empFirstName = answers.empFirstName;
+                const empLastName = answers.empLastName;
+                const empDeptID = answers.empDeptID;
+                const empRoleID = answers.empRoleID;
+                const empMgrID = empMgrID;
+                addEmployeeToDatabase(empFirstName, empLastName, empDeptID, empRoleID, empMgrID);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+            return empFirstName, empLastName, empDeptID, empRoleID, empMgrID;
+        case "Update An Employee Role":
+            query = inquirer.prompt(updateRole)
+            .then((answers) => { // Build out functionality
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+            break;
+    }
+};
+
+function addDepartmentToDatabase(deptName, deptID) {
+    const query = 'INSERT INTO departments (deptName, deptID) VALUES (?, ?)';
+    connection.query(query, [deptName, deptID], (err, result) => {
+        if (err) {
+            console.error('Error adding department:', err);
+            return;
+        }
+        console.log('Department added successfully!');
+    });
+}
+
+function addRoleToDatabase(roleName, roleID, roleSalary, roleDeptID) {
+    const query = 'INSERT INTO roles (roleName, roleID, roleSalary, roleDeptID) VALUES (?, ?, ?, ?)';
+    connection.query(query, [roleName, roleID, roleSalary, roleDeptID], (err, result) => {
+        if (err) {
+            console.error('Error adding role:', err);
+            return;
+        }
+        console.log('Role added successfully!');
+    });
+}
+
+function addEmployeeToDatabase(empFirstName, empLastName, empDeptID, empRoleID, empMgrID) {
+    const query = 'INSERT INTO roles (empFirstName, empLastName, empDeptID, empRoleID, empMgrID) VALUES (?, ?, ?, ?, ?)';
+    connection.query(query, [empFirstName, empLastName, empDeptID, empRoleID, empMgrID], (err, result) => {
+        if (err) {
+            console.error('Error adding role:', err);
+            return;
+        }
+        console.log('Employee added successfully!');
+    });
+}
+
+function closeConnection() {
+    connection.end(err => {
+        if (err) {
+            console.error('Error closing the connection:', err);
+            return;
+        }
+        console.log('Connection closed');
+    });
+}
+
+module.exports = startProcess;
